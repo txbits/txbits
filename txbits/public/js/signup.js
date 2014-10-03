e3b00c922cd2b9f5d537c6eb6b0fe4fcb74ef5e8
Kirk Zathey <zathey@gmail.com>
2014-10-03 01:27:58 -0400

    Revise signup email verification

--- txbits/public/js/signup.js
+++ txbits/public/js/signup.js
@@ -1,16 +1,9 @@
 $(function(){
-    var $email = $('#email');
-    var $email2 = $('#email2');
-    var match_error = false;
-    $('#signup-form').on('submit', function(e) {
-        if ($email.val() != $email2.val()) {
+    $('#signup-form').submit(function(e) {
+        if ($('#email').val() != $('#email2').val()) {
             e.preventDefault();
-            e.returnValue = false;
-            if (!match_error) {
-                $('#email2_field').addClass("has-error").append(
-                    '<span class="help-block">Emails do not match</span>');
-                match_error = true;
-            }
+            $('#email2_field').addClass("has-error");
+            $('#email2_error').show();
         }
     });
 });
