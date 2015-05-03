ac6fa6852b80bea84be3171ce2a017552d1cdfd4
Viktor Stanchev <me@viktorstanchev.com>
2015-05-03 23:16:16 +0200

    add copyright headers to some files that were missing it

--- txbits/app/service/AnormParsers.scala
+++ txbits/app/service/AnormParsers.scala
@@ -1,3 +1,7 @@
+// Copyright (c) 2014 Viktor Stanchev & Kirk Zathey.
+// This file is licensed under the Affero General Public License version 3 or later,
+// see the accompanying file COPYING or http://www.gnu.org/licenses/agpl.html.
+
 package service
 
 import anorm._
