0b69e3a9031163b3fc8b25f6e1a4c21eed6caa9e
Viktor Stanchev <me@viktorstanchev.com>
2015-06-16 09:35:49 -0400

    fix bug and make spinner look better

--- txbits/public/js/pager.js
+++ txbits/public/js/pager.js
@@ -43,7 +43,12 @@ Pager.make_easy_pager = function(api, $parent, table_selector, body_selector, sp
             if(preprocess) {
                 data = preprocess(data);
             }
+        }
 
+        var html = template(data);
+        $parent.html(html);
+
+        if(data.length > 0) {
             new Pager($(table_selector),
                 $(body_selector),
                 $(spinner_selector),
@@ -67,7 +72,5 @@ Pager.make_easy_pager = function(api, $parent, table_selector, body_selector, sp
                 last_timestamp
             );
         }
-        var html = template(data);
-        $parent.html(html);
     });
 };
