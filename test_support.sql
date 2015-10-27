eb91446830e1b0eeaa3cd7e284eefed8ef17d522
Jim Nasby <Jim.Nasby@BlueTreble.com>
2015-10-27 18:30:08 -0500

    Require test_factory_pgtap

--- test_support.sql
+++ test_support.sql
@@ -3,5 +3,6 @@ GRANT USAGE ON SCHEMA tap TO public;
 CREATE EXTENSION pgtap SCHEMA tap;
 SET search_path = "$user", public, tap;
 CREATE EXTENSION test_factory;
+CREATE EXTENSION test_factory_pgtap;
 
 -- vi: noexpandtab sw=4 ts=4
