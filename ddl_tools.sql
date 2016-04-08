c66cfca3089ffd75cc708bbffbfa3ff60af2c8fd
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-04-07 21:11:44 -0500

    Refactor ddl_tools.exec()

--- ddl_tools.sql
+++ ddl_tools.sql
@@ -4,38 +4,7 @@ SELECT CASE WHEN :'DB_DIR' = '' THEN '.' ELSE :'DB_DIR' END AS DB_DIR
 \gset
 
 CREATE SCHEMA ddl_tools;
-CREATE FUNCTION ddl_tools.exec(
-  sql text
-  , as_role name DEFAULT NULL
-) RETURNS void LANGUAGE plpgsql AS $body$
-DECLARE
-  ci_orig_user CONSTANT name := quote_ident(current_user);
-BEGIN
-  IF as_role IS NOT NULL THEN
-    RAISE DEBUG 'EXECUTE AS %: %', as_role, sql;
-    EXECUTE 'SET ROLE ' || quote_ident( as_role );
-    EXECUTE sql;
-    EXECUTE 'SET ROLE ' || ci_orig_user;
-  ELSE
-    RAISE DEBUG 'EXECUTE: %', sql;
-    EXECUTE sql;
-  END IF;
-END
-$body$;
-CREATE FUNCTION ddl_tools.exec(
-  sql text[]
-  , as_role name DEFAULT NULL
-) RETURNS void LANGUAGE plpgsql AS $body$
-DECLARE
-  v_sql text;
-BEGIN
-  FOREACH v_sql IN ARRAY sql LOOP
-    PERFORM ddl_tools.exec( v_sql, as_role );
-  END LOOP;
-END
-$body$;
-
--- This is a db_tools file!
+\i :DB_DIR/functions/ddl_tools.exec.sql
 
 CREATE FUNCTION ddl_tools.role__create(
   role_name  name
