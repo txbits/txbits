1d136552bea0148a38eb52eb727fb7902c8e3616
Jim Nasby <Jim.Nasby@BlueTreble.com>
2017-08-20 18:46:23 -0500

    Add default privileges

--- txbits/db/deploy/INIT.roles.sql
+++ txbits/db/deploy/INIT.roles.sql
@@ -6,10 +6,16 @@ SET ROLE su;
 
 -- Non-login roles
 SELECT ddl_tools.role__create( 'txbits__' || suffix )
-	FROM unnest( '{owner, read, read_pii, app, dev}'::text[] ) AS suffix
+	FROM unnest( '{owner, read, app, dev}'::text[] ) AS suffix
 ;
-GRANT txbits__read TO txbits__read_pii;
-GRANT txbits__read_pii TO txbits__app;
+
+/*
+ * NOTE: this is normally done by some functions that make it easy to create
+ * new schemas, so some of it's a bit redundant.
+ */
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO txbits__read, txbits__dev;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO txbits__app;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO txbits__app;
 
 -- Login roles
 /*
